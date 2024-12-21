42938841643bd2d68be6c7bf2428b21a242f7913
barry <pika1361418399@gmail.com>
2024-12-21 14:37:49 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
         title: 'TOOCAA Support Center',
         logo: {
           alt: 'TOOCAA Logo',
-          src: 'http://wiki-toocaa.oss-cn-hongkong.aliyuncs.com/LOGO111.png',
+          src: 'http://wiki-toocaa.oss-cn-hongkong.aliyuncs.com/LOGO.png',
           href: 'https://www.toocaa.com/',
           target:  '_self',
         },
