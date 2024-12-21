d2c21c05d93a19f9c487c040237b31e1c916e168
barry <pika1361418399@gmail.com>
2024-12-21 14:28:52 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'LEARN',
+  title: 'TOOCAA Support Center',
   tagline: 'Focus on micro:bit accessories!',
   url: 'https://wiki.toocaa.com/',
   baseUrl: '/',
